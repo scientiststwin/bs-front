678a3f199439bca2027f6daf413aa67f7e09fda8
Mohammad Twin <scientists.twin@gmail.com>
2024-02-02 17:26:34 +0400

    feat: envs were added to the project
    use env to specify baseURL of axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import AppRouter from "./routes/root";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
